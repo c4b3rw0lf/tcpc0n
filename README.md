b64cb5e4347abc97a5a78e44711ebdda6b8baaf4
c4b3rw0lf <m4dbb33f@gmail.com>
2017-10-07 19:03:10 +0200

    commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# tcpc0n
+# TCP Connection Client
+
+# INSTALLATION
+# Linux
+Debian based systems :
+
+sudo apt-get install python-pip
+
+cd /setup 
+
+sudo pip install -r requirements.txt 
+
+edit the script to fit your needs(host,port,data to send)
+
+python tcpc0n.py
+
---
+++ tcpc0n.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from pyfiglet import Figlet
+f = Figlet(font='slant')
+print f.renderText('tcpc0n')
+
+print '##### TCP Connection Client #####'
+print '##### By c4b3rw0lf #####'
+
+import socket 
+
+target_host = "127.0.0.1" 
+target_port = 80
+
+# Create socket object 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect tcpc0n to target
+client.connect((target_host,target_port))
+
+# Send data to target
+client.send("HEAD / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n")
+
+# Receive data from target
+response = client.recv(4096)
+
+print response
+
+
+
+
